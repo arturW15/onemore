f777729a77198ed7764675642c6bed3d38ee4308
arturW15 <126724378+arturW15@users.noreply.github.com>
2023-03-02 23:53:38 +0200

    Add files via upload

---
+++ project.js
@@ -0,0 +1,94 @@
+var allwind = document.querySelector('.allwind');
+var allwind2 = document.querySelector('.allwind2');
+var allwind3 = document.querySelector('.allwind3');
+var allwind4 = document.querySelector('.allwind4');
+var allwind5 = document.querySelector('.allwind5');
+var allwind6 = document.querySelector('.allwind6');
+var allwind7 = document.querySelector('.allwind7');
+var allwind8 = document.querySelector('.allwind8');
+var allwind9 = document.querySelector('.allwind9');
+
+var allwindh = document.querySelector('.allwindh');
+
+var btn = document.getElementById('buttonok');
+var btn2 = document.getElementById('buttonok2');
+var btn3 = document.getElementById('buttonok3');
+var btn4 = document.getElementById('buttonok4');
+var btn5 = document.getElementById('buttonok5');
+var btn6 = document.getElementById('buttonok6');
+var btn7 = document.getElementById('buttonok7');
+var btn8 = document.getElementById('buttonok8');
+var btn9 = document.getElementById('buttonok9');
+
+var btnh = document.getElementById('buttonokh');
+var hidtext = document.getElementById('simtexti');
+
+function forhidtextfunction() {
+  hidtext.style.display = "block";
+}
+
+btn.onclick = function() {
+  allwind.close();
+}
+btn2.onclick = function() {
+  allwind2.close();
+}
+btn3.onclick = function() {
+  allwind3.close();
+}
+btn4.onclick = function() {
+  allwind4.close();
+}
+btn5.onclick = function() {
+  allwind5.close();
+}
+btn6.onclick = function() {
+  allwind6.close();
+}
+btn7.onclick = function() {
+  allwind7.close();
+}
+btn8.onclick = function() {
+  allwind8.close();
+}
+btn9.onclick = function() {
+  allwind9.close();
+}
+btnh.onclick = function() {
+  allwindh.close();
+}
+
+
+function fucti1() {
+  allwind.showModal();
+}
+function fucti2() {
+  allwind2.showModal();
+}
+function fucti3() {
+  allwind3.showModal();
+}
+function fucti4() {
+  allwind4.showModal();
+}
+function fucti5() {
+  allwind5.showModal();
+}
+function fucti6() {
+  allwind6.showModal();
+}
+function fucti7() {
+  allwind7.showModal();
+}
+function fucti8() {
+  allwind8.showModal();
+}
+function fucti9() {
+  allwind9.showModal();
+}
+function fuctsy2() {
+  window.location = 'https://www.youtube.com/'
+}
+function fuctsy1() {
+  allwindh.showModal();
+}
